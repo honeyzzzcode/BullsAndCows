58fcf63f040de3d1fdd0730851a3bff71f312524
Zlata <zlata2000@inbox.lv>
2021-04-29 10:03:57 +0300

    game logic

---
+++ src/All.java
@@ -0,0 +1,47 @@
+public class All {
+
+    private int nr ;
+
+
+
+    private String guess ;
+    private int bulls ;
+    private int cows ;
+    public int getNr() {
+        return nr;
+    }
+
+    public void setNr(int nr) {
+        this.nr = nr;
+    }
+
+    public String getGuess() {
+        return guess;
+    }
+
+    public void setGuess(String guess) {
+        this.guess = guess;
+    }
+
+    public int getBulls() {
+        return bulls;
+    }
+
+    public void setBulls(int bulls) {
+        this.bulls = bulls;
+    }
+
+    public int getCows() {
+        return cows;
+    }
+
+    public void setCows(int cows) {
+        this.cows = cows;
+    }
+
+
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,67 @@
+import java.nio.charset.Charset;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-        
+        boolean running = true;
+        int count = 0;
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> myNumbers;
+        var rand = new Random();
+        var set = new LinkedHashSet<Integer>();
+        while (set.size() < 4) {
+            var n = rand.nextInt(10);
+            set.add(n);
+        }
+        myNumbers = List.copyOf(set);
+        System.out.println(myNumbers);
+
+        System.out.println("Welcome to bulls and cows game ! " );
+        do {
+            System.out.println("Try to guess the number:");
+
+            var n1 = scanner.nextInt();
+            var n2 = scanner.nextInt();
+            var n3 = scanner.nextInt();
+            var n4 = scanner.nextInt();
+            var guess ="" + n1 + n2 + n3 + n4 ;
+
+            var userNumbers = List.of(n1,n2,n3,n4);
+            System.out.println(userNumbers);
+
+            int bulls = 0;
+            int cows = 0;
+            for (int j = 0; j < 4; j++) {
+                int myN = myNumbers.get(j);
+                for (int z = 0; z < 4; z++) {
+                    int usrN = userNumbers.get(z);
+                    if (myN == usrN) {
+                        if (j == z) {
+                            bulls++;
+                        } else {
+                            cows++;
+                        }
+
+                    }
+                }
+            }
+            var all = new All();
+            count++;
+            all.setGuess(guess);
+            all.setNr(count);
+            all.setBulls(bulls);
+            all.setCows(cows);
+            System.out.println("bulls : "+bulls+" and cows: "+ cows);
+            if (bulls == 4) {
+
+                System.out.println("Congratulations!You won");
+                running=false;
+            }
+        }while (running);
+
+
+
     }
 }
